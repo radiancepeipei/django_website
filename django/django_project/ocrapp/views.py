daaf051e17a5a6e5605fb4da70e7aafc7f1bc9fc
radiance <radiance6252@gmail.com>
2019-11-28 13:52:22 +0800

    button bootstrap style

--- django/django_project/ocrapp/views.py
+++ django/django_project/ocrapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,HttpResponse
+from django.template.loader import get_template
 
 # Create your views here.
 def ocrapp(request):
     return render(request, 'index.html', {
         'data': "Hello Django ",
     })
+
+def ocr_button(request):
+    return render(request,'index.html')
